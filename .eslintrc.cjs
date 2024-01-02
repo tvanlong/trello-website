bce850a0d1c4fbf5275454cfa1cebd72fa2a19c4
tvanlong <longvan1173@gmail.com>
2024-01-02 21:43:15 +0700

    Update Eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -10,11 +10,31 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-hooks', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/prop-types': 0,
+
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'no-lonely-if': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 1 }],
+    'no-multi-spaces': 'warn',
+    'space-before-blocks': ['warn', 'always'],
+    'object-curly-spacing': ['warn', 'always'],
+    indent: ['warn', 2],
+    quotes: ['warn', 'single'],
+    'array-bracket-spacing': 'warn',
+    'linebreak-style': 'off',
+    'no-unexpected-multiline': 'warn',
+    'comma-dangle': 'warn',
+    'comma-spacing': 'warn',
+    'arrow-spacing': 'warn',
   },
-}
+};
