dfe89b40d32b47f02e1c370eabab90596e29b847
tvanlong <longvan1173@gmail.com>
2024-01-03 22:21:50 +0700

    config theme

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#556cd6'
+    },
+    secondary: {
+      main: '#19857b'
+    },
+    error: {
+      main: red.A400
+    },
+    text: {
+      primary: '#000',
+      secondary: red[500]
+    }
+  }
+})
+
+export default theme
